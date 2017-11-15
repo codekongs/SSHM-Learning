f247d4b025cd3b434792469dd9541e67d83e1ffd
codekongs <531173160@qq.com>
2017-11-15 10:15:12 +0800

    Hibernate单向多对一映射完成

--- Hibernate/HibernateMapping/src/test/java/TestManyToOne.java
+++ Hibernate/HibernateMapping/src/test/java/TestManyToOne.java
@@ -6,6 +6,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import bean.Department;
+import bean.User;
+
 /**
  * Created by 尚振鸿 on 17-11-14. 22:28
  * mail:szh@codekong.cn
@@ -27,23 +30,39 @@ public class TestManyToOne {
 
     @Test
     public void manyToOneTest(){
+        Department department = new Department("综合事业部");
+        User user1 = new User("小红", "123456", department);
+        User user2 = new User("小白", "56789", department);
+        mSession.save(department);
+        mSession.save(user1);
+        mSession.save(user2);
 
     }
 
     @Test
-    public void oneToManyGet(){
+    public void manyToOneGet(){
+        Department department = mSession.get(Department.class, 1);
+        System.out.println(department);
+        User user = mSession.get(User.class, department.getCode());
+        System.out.println(user);
 
+        User user1 = mSession.get(User.class, 1);
+        System.out.println(user.getDepartment());
     }
 
 
     @Test
-    public void oneToManyUpdate(){
-
+    public void manyToOneUpdate(){
+        Department department = new Department("无线事业群");
+        User user1 = mSession.get(User.class, 1);
+        user1.setDepartment(department);
+        mSession.update(user1);
     }
 
     @Test
-    public void oneToManyDel(){
-
+    public void manyToOneDel(){
+        Department department = mSession.get(Department.class, 2);
+        mSession.delete(department);
     }
 
 
